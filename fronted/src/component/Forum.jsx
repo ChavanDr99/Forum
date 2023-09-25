52878e08ea4ce5dbaeb24eee4e7cdd11440331cc
shivraj <sk40fs@gmail.com>
2023-09-25 13:48:53 +0530

    'responsive'

--- fronted/src/component/Forum.jsx
+++ fronted/src/component/Forum.jsx
@@ -7,7 +7,7 @@ const Forum = () => {
     <div className="w-full min-w-fit">
       <Header/>
      <div className="flex justify-center py-10 bg-opacity-5 bg-black min-w-min w-full">
-    <div className="flex flex-row px-10 w-full max-w-screen-xl ">
+    <div className="flex flex-row max-md:flex-col px-2 w-full max-w-screen-xl ">
       <SideImage/>
       <Feed/>
     </div>
